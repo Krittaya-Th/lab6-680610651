8729150491975504797292e86e59b0e99519e10c
Krittaya-Th <krittayaclf9028@gmail.com>
2025-12-09 19:02:36 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int n=0,e=0,o=0,i=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >>n;
+    while (n!=0){
+        if(n%2==0){
+            e=e+1;
+        }else{
+            o=o+1;
+        }
+    cout <<"Enter an integer: ";
+    cin >> n;
+    i++;
+        }
+    cout << "#Even numbers = "<<e<<"\n";
+    cout << "#Odd numbers = "<<o;
     return 0;
 }
