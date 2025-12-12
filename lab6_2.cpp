de6b5badd0fd0ee4b4ba56c569f558d84ed806ae
Krittaya-Th <krittayaclf9028@gmail.com>
2025-12-12 15:31:10 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-#include <c.math>
+#include <cmath>
 using namespace std;
 double deg2rad(double d){
-    double r=((d/360)*(2*M_PI));
+    double r=(d/180)*M_PI;
     return r;
 }
 double rad2deg(double r){
-    double d=((r/(2*M_PI))*360);
+     double d=((r/(M_PI))*180);
+    return d;
 }
-findXComponent
+double findXComponent(double w1,double w2,double m1,double m2){
+    double x=((w1*cos(m1)))+(w2*cos(m2));
+    return x;
+}
+double findYComponent(double l1,double l2,double d1,double d2){
+    double y=((l1*sin(d1)))+(l2*sin(d2));
+    return y;
+}
+double pythagoras(double x,double y){
+    double p=sqrt((pow(x,2)+pow(y,2)));
+    return p;
+}
+void showResult(double p,double c){
+    cout <<"Length of the resultant vector = "<<p<<"\n";
+    cout <<"Direction of the resultant vector (deg) = "<<c;
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
